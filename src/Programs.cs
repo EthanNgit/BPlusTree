c6d8e9339132fe00898f2b72f4797d4827b7aa82
Ethan North <northethana@gmail.com>
2025-01-09 10:29:43 -0600

    Update Unit test + Deletion unit test

--- src/Programs.cs
+++ src/Programs.cs
@@ -5,47 +5,45 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("---------------------------------------------------------------");
+        // Console.WriteLine("---------------------------------------------------------------");
 
-        BPTree tree = new BPTree(4);
-        var treeSize = 10;
+        // BPTree tree = new BPTree(4);
+        // var treeSize = 10;
 
-        var arr = new int[treeSize];
-        for (int i = 1; i <= treeSize; i++)
-        {
-            arr[i - 1] = i;
-        }
+        // var arr = new int[treeSize];
+        // for (int i = 1; i <= treeSize; i++)
+        // {
+        //     arr[i - 1] = i;
+        // }
 
-        Random random = new Random();
+        // Random random = new Random();
 
-        for (int i = 0; i < arr.Length - 1; ++i)
-        {
-            int r = random.Next(i, arr.Length);
-            (arr[r], arr[i]) = (arr[i], arr[r]);
-        }
+        // for (int i = 0; i < arr.Length - 1; ++i)
+        // {
+        //     int r = random.Next(i, arr.Length);
+        //     (arr[r], arr[i]) = (arr[i], arr[r]);
+        // }
 
-        foreach (var i in arr)
-        {
-            tree.Insert(i);
-            Console.Write(i + " ");
-        }
+        // foreach (var i in arr)
+        // {
+        //     tree.Insert(i);
+        //     Console.Write(i + " ");
+        // }
 
-        foreach (var i in arr)
-        {
-            Console.WriteLine($"Deleting {i}\n");
+        // foreach (var i in arr)
+        // {
+        //     Console.WriteLine($"Deleting {i}\n");
 
-            tree.Delete(i);
+        //     tree.Delete(i);
 
-            tree.PrintLevelOrderTraversal();
-            tree.PrintLeafListTraversal(1);
+        //     tree.PrintLevelOrderTraversal();
+        //     tree.PrintLeafListTraversal(1);
 
-            Console.WriteLine("\n");
-        }
+        //     Console.WriteLine("\n");
+        // }
 
-
-
-        // TestBPTree.Run_All_Tests();
-        Console.WriteLine("---------------------------------------------------------------");
+        // Console.WriteLine("---------------------------------------------------------------");
+        TestBPTree.Run_All_Tests();
 
     }
 }
--- src/test/BPTreeTests.cs
+++ src/test/BPTreeTests.cs
@@ -4,116 +4,207 @@ namespace Norbula.BPTree.BPTreeTest
 {
     public class TestBPTree
     {
+        private const int _testLoops = 10; // if random is used, this will be
+        private const int _randomStartingSeed = 77;
+
         public static void Run_All_Tests()
         {
-            Insert_100Elements_ChecksSortForward();
-            Insert_100Elements_ChecksSortBackward();
+            Insert_Checks_Exists();
+            Insert_100Elements_Checks_SortForward();
+            Insert_100Elements_Checks_SortBackward();
+            Insert_100Elements_Checks_CompleteDeletion();
         }
 
-        public static void Insert_100Elements_ChecksSortForward()
+        public static void Insert_Checks_Exists()
         {
+            string testName = "Insert_Checks_Exists";
+
             try
             {
-                // Arrange
                 BPTree tree = new BPTree(4);
-                var arr = new int[100];
+                tree.Insert(1);
 
-                for (int i = 1; i <= 100; i++) arr[i - 1] = i;
-
-                Random random = new Random();
-                for (int i = 0; i < arr.Length - 1; ++i)
+                if (tree.Search(1) != null)
                 {
-                    int r = random.Next(i, arr.Length);
-                    (arr[r], arr[i]) = (arr[i], arr[r]);
+                    Console.WriteLine($"PASS: {testName}");
+
                 }
-
-                // Act
-                foreach (var i in arr) tree.Insert(i);
-                var nodeOne = tree.Search(1);
-
-                // Assert
-                var arrOne = new int[100];
-                var j = 0;
-                while (nodeOne != null)
+                else
                 {
-                    var keys = nodeOne.Keys;
+                    Console.WriteLine($"FAILED: {testName}");
 
-                    for (int k = 0; k < nodeOne.KeysCount; k++)
-                    {
-                        arrOne[j] = keys[k];
-
-                        if (j > 0 && j < 100 && arrOne[j] < arrOne[j - 1])
-                        {
-                            Console.WriteLine("FAILED: Insert_100Elements_ChecksSortForward");
-                            return;
-                        }
-
-                        j++;
-                    }
-
-                    nodeOne = nodeOne.Next;
                 }
-
-                Console.WriteLine("PASS: Insert_100Elements_ChecksSortForward");
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"FAILED: Insert_100Elements_ChecksSortForward {ex}");
+                Console.WriteLine($"FAILED: {testName} {ex}");
+
             }
+
         }
 
-        public static void Insert_100Elements_ChecksSortBackward()
+        public static void Insert_100Elements_Checks_SortForward()
         {
+            string testName = "Insert_100Elements_Checks_SortForward";
+
             try
             {
-                // Arrange
-                BPTree tree = new BPTree(4);
-                var arr = new int[100];
-
-                for (int i = 1; i <= 100; i++) arr[i - 1] = i;
-
-                Random random = new Random();
-                for (int i = 0; i < arr.Length - 1; ++i)
+                for (int b = 0; b < _testLoops; b++)
                 {
-                    int r = random.Next(i, arr.Length);
-                    (arr[r], arr[i]) = (arr[i], arr[r]);
-                }
+                    // Arrange
+                    BPTree tree = new BPTree(4);
+                    var arr = new int[100];
 
-                // Act
-                foreach (var i in arr) tree.Insert(i);
-                var nodeOne = tree.Search(100);
+                    for (int i = 1; i <= 100; i++) arr[i - 1] = i;
 
-                // Assert
-                var arrOne = new int[100];
-                var j = 0;
-                while (nodeOne != null)
-                {
-                    var keys = nodeOne.Keys;
-
-                    for (int k = nodeOne.KeysCount - 1; k >= 0; k--)
+                    Random random = new Random(_randomStartingSeed + b);
+                    for (int i = 0; i < arr.Length - 1; ++i)
                     {
-                        arrOne[j] = keys[k];
-                        if (j > 0 && j < 100 && arrOne[j] > arrOne[j - 1])
-                        {
-                            Console.WriteLine("FAILED: Insert_100Elements_ChecksSortBackward");
-                            return;
-                        }
-
-                        j++;
+                        int r = random.Next(i, arr.Length);
+                        (arr[r], arr[i]) = (arr[i], arr[r]);
                     }
 
-                    nodeOne = nodeOne.Prev;
+                    // Act
+                    foreach (var i in arr) tree.Insert(i);
+                    var nodeOne = tree.Search(1);
+
+                    // Assert
+                    var arrOne = new int[100];
+                    var j = 0;
+                    while (nodeOne != null)
+                    {
+                        var keys = nodeOne.Keys;
+
+                        for (int k = 0; k < nodeOne.KeysCount; k++)
+                        {
+                            arrOne[j] = keys[k];
+
+                            if (j > 0 && j < 100 && arrOne[j] < arrOne[j - 1])
+                            {
+                                Console.WriteLine($"FAILED: {testName} [seed: {_randomStartingSeed + b}]");
+                                return;
+                            }
+
+                            j++;
+                        }
+
+                        nodeOne = nodeOne.Next;
+                    }
+
+                    continue; // passed this iteration
                 }
 
-                Console.WriteLine("PASS: Insert_100Elements_ChecksSortBackward");
+                Console.WriteLine($"PASS: {testName}");
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"FAILED: Insert_100Elements_ChecksSortBackward {ex}");
+                Console.WriteLine($"FAILED: {testName} {ex}");
             }
         }
 
+        public static void Insert_100Elements_Checks_SortBackward()
+        {
+            string testName = "Insert_100Elements_Checks_SortBackward";
 
+            try
+            {
+                // Arrange
+                for (int b = 0; b < _testLoops; b++)
+                {
+                    BPTree tree = new BPTree(4);
+                    var arr = new int[100];
+
+                    for (int i = 1; i <= 100; i++) arr[i - 1] = i;
+
+                    Random random = new Random(_randomStartingSeed + b);
+                    for (int i = 0; i < arr.Length - 1; ++i)
+                    {
+                        int r = random.Next(i, arr.Length);
+                        (arr[r], arr[i]) = (arr[i], arr[r]);
+                    }
+
+                    // Act
+                    foreach (var i in arr) tree.Insert(i);
+                    var nodeOne = tree.Search(100);
+
+                    // Assert
+                    var arrOne = new int[100];
+                    var j = 0;
+                    while (nodeOne != null)
+                    {
+                        var keys = nodeOne.Keys;
+
+                        for (int k = nodeOne.KeysCount - 1; k >= 0; k--)
+                        {
+                            arrOne[j] = keys[k];
+                            if (j > 0 && j < 100 && arrOne[j] > arrOne[j - 1])
+                            {
+                                Console.WriteLine($"FAILED: {testName} [seed: {_randomStartingSeed + b}]");
+                                return;
+                            }
+
+                            j++;
+                        }
+
+                        nodeOne = nodeOne.Prev;
+                    }
+
+                    continue; // passed this iteration
+                }
+
+                Console.WriteLine($"PASS: {testName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"FAILED: {testName} {ex}");
+            }
+        }
+
+        public static void Insert_100Elements_Checks_CompleteDeletion()
+        {
+            string testName = "Insert_100Elements_Checks_CompleteDeletion";
+            try
+            {
+                // Arrange
+                for (int b = 0; b < _testLoops; b++)
+                {
+                    BPTree tree = new BPTree(4);
+                    var arr = new int[100];
+                    for (int i = 1; i <= 100; i++) arr[i - 1] = i;
+
+                    Random random = new Random(_randomStartingSeed + b);
+                    for (int i = 0; i < arr.Length - 1; ++i)
+                    {
+                        int r = random.Next(i, arr.Length);
+                        (arr[r], arr[i]) = (arr[i], arr[r]);
+                    }
+
+                    // Act
+                    foreach (var i in arr) tree.Insert(i);
+                    var nodeOne = tree.Search(1);
+
+                    // Assert
+                    for (int i = 0; i < arr.Length - 1; ++i)
+                    {
+                        tree.Delete(i);
+
+                        if (tree.Search(i) != null)
+                        {
+                            Console.WriteLine($"FAILED: {testName} [seed: {_randomStartingSeed + b}]");
+                        }
+                    }
+
+                    continue; // passed this iteration
+                }
+
+                Console.WriteLine($"PASS: {testName}");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"FAILED: {testName} {ex}");
+            }
+        }
     }
 
 }
